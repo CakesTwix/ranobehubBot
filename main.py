6046b17197bf2ff960a17eb505d20440c49ae504
CakesTwix <oleg.kiryazov@gmail.com>
2021-01-13 19:49:34 +0000

    main: forgot one import..

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from aiogram import executor
 import asyncio
 from config import initBot, dp
 from handlers import check_callback_query, favorites, last, popular, search, send, start, subscribe_callback_query
+from state import getChapter
 
 
 if __name__ == "__main__":
