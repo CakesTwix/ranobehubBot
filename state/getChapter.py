1e389630512c6b7ea5d01bc3fd6538f448adbb15
CakesTwix <oleg.kiryazov@gmail.com>
2021-06-23 12:36:43 +0300

    state: fix error with int()

--- state/getChapter.py
+++ state/getChapter.py
@@ -50,15 +50,20 @@ async def giveMeChapters(callback_query: CallbackQuery, callback_data: CallbackD
 async def giveMeChapter(message: Message, state: FSMContext):
     data = await state.get_data()
     Link = InlineKeyboardMarkup()
-
-    Link.add(InlineKeyboardButton('Ссылка на главу',
+    if message.text.isdigit():    
+        try:
+            Link.add(InlineKeyboardButton('Ссылка на главу',
                                   url=data[int(data[0]["numVolume"])]["chapters"][int(message.text) - 1]["url"]))
-    Link.add(InlineKeyboardButton('Отправить сюда',
+            Link.add(InlineKeyboardButton('Отправить сюда',
                                   callback_data=telegraData.new(action='telegra', url=data[int(data[0]["numVolume"])]["chapters"][int(message.text) - 1]["url"][8:])))
-    await bot.send_message(message.chat.id, data[int(data[0]["numVolume"])]["chapters"][int(message.text) - 1]["name"],
+            await bot.send_message(message.chat.id, data[int(data[0]["numVolume"])]["chapters"][int(message.text) - 1]["name"],
                            reply_markup=Link)
-    logger.info(f'{message.from_user.full_name}: Ссылка на главу')
-
+            logger.info(f'{message.from_user.full_name}: Ссылка на главу')
+        except IndexError:
+            await message.reply("Число ушло за рамки дозволенного.")
+    else:
+        await message.reply("Введите правильно номер главы.")
+    
 
 @dp.callback_query_handler(telegraData.filter(action='telegra'), state=check.getChapter)
 async def telegra_callback_query(callback_query: CallbackQuery, callback_data: CallbackData, state: FSMContext):
